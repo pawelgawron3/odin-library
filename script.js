ebb0f1faa11a621e7364ee643f3f95623c3a271b
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-08-30 11:57:20 +0200

    Add displayBooks function, add example books for testing

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let library = [];
+let main = document.querySelector("main");
+
+let library = [
+  {
+    id: crypto.randomUUID(),
+    title: "Wiedźmin",
+    author: "Andrzej Sapkowski",
+    pages: 350,
+    hasBeenRead: "true",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Zbrodnia i kara",
+    author: "Fiodor Dostojewski",
+    pages: 600,
+    hasBeenRead: "false",
+  },
+];
 
 function Book(title, author, pages, hasBeenRead) {
   if (!target.new) {
@@ -15,4 +32,43 @@ function Book(title, author, pages, hasBeenRead) {
 function addBookToLibrary(title, author, pages, hasBeenRead) {
   let book = new Book(title, author, pages, hasBeenRead);
   library.push(book);
+}
+
+function displayBooks(booksArray) {
+  booksArray.forEach((book) => {
+    let div = document.createElement("div");
+    div.id = book.id;
+    div.style.backgroundColor = "#9580ff";
+    div.style.padding = "10px";
+
+    for (let i = 0; i < 4; i++) {
+      let p = document.createElement("p");
+      p.style.color = "whitesmoke";
+
+      switch (i) {
+        case 0:
+          p.className = "title";
+          p.textContent = book.title;
+          break;
+
+        case 1:
+          p.className = "author";
+          p.textContent = book.author;
+          break;
+
+        case 2:
+          p.className = "pages";
+          p.textContent = book.pages;
+          break;
+
+        case 3:
+          p.className = "hasBeenRead";
+          p.textContent = book.hasBeenRead;
+          break;
+      }
+      div.appendChild(p);
+    }
+
+    main.appendChild(div);
+  });
 }
