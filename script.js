cc9ab108054d8235f6771d3b17792564e0f3472b
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-08-30 14:31:08 +0200

    Update the script logic, add event listeners

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-let main = document.querySelector("main");
+let library = [];
 
-let library = [
-  {
-    id: crypto.randomUUID(),
-    title: "Wiedźmin",
-    author: "Andrzej Sapkowski",
-    pages: 350,
-    hasBeenRead: "true",
-  },
-  {
-    id: crypto.randomUUID(),
-    title: "Zbrodnia i kara",
-    author: "Fiodor Dostojewski",
-    pages: 600,
-    hasBeenRead: "false",
-  },
-];
+const addBook = document.querySelector("#addBook");
+const suggestBook = document.querySelector("#suggestBook");
+const removeAllBooks = document.querySelector("#removeAllBooks");
+
+const dialog = document.querySelector("#addBookDialog");
+const bookForm = document.querySelector("#addBookForm");
+const formCancelBookBtn = dialog.querySelector("button#cancelDialog");
 
 function Book(title, author, pages, hasBeenRead) {
-  if (!target.new) {
+  if (!new.target) {
     throw console.error("Only with 'new' keyword!");
   } else {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.hasBeenRead = hasBeenRead;
+    this.hasBeenRead = hasBeenRead ? "Yes" : "No";
     this.id = crypto.randomUUID();
   }
 }
@@ -35,11 +26,14 @@ function addBookToLibrary(title, author, pages, hasBeenRead) {
 }
 
 function displayBooks(booksArray) {
+  let main = document.querySelector("main");
+
+  main.innerHTML = "";
+
   booksArray.forEach((book) => {
     let div = document.createElement("div");
     div.id = book.id;
-    div.style.backgroundColor = "#9580ff";
-    div.style.padding = "10px";
+    div.className = "book-card";
 
     for (let i = 0; i < 4; i++) {
       let p = document.createElement("p");
@@ -48,27 +42,65 @@ function displayBooks(booksArray) {
       switch (i) {
         case 0:
           p.className = "title";
-          p.textContent = book.title;
+          p.textContent = `Title: ${book.title}`;
           break;
 
         case 1:
           p.className = "author";
-          p.textContent = book.author;
+          p.textContent = `Author: ${book.author}`;
           break;
 
         case 2:
           p.className = "pages";
-          p.textContent = book.pages;
+          p.textContent = `Number of pages: ${book.pages}`;
           break;
 
         case 3:
           p.className = "hasBeenRead";
-          p.textContent = book.hasBeenRead;
+          p.textContent = `Read: ${book.hasBeenRead}`;
           break;
       }
       div.appendChild(p);
     }
 
+    let deleteBookBtn = document.createElement("button");
+    deleteBookBtn.className = "deleteSingleBook";
+    deleteBookBtn.type = "button";
+
+    let i = document.createElement("i");
+    i.className = "mdi mdi-delete";
+
+    deleteBookBtn.appendChild(i);
+
+    div.appendChild(deleteBookBtn);
+
     main.appendChild(div);
   });
 }
+
+removeAllBooks.addEventListener("click", () => {
+  library = [];
+  displayBooks(library);
+});
+
+addBook.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+formCancelBookBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let title = dialog.querySelector("#book-title").value;
+  let author = dialog.querySelector("#book-author").value;
+  let pages = dialog.querySelector("#book-pages").value;
+  let hasBeenRead = dialog.querySelector("#book-hasBeenRead").checked;
+
+  addBookToLibrary(title, author, pages, hasBeenRead);
+  displayBooks(library);
+  dialog.close();
+  bookForm.reset();
+});
